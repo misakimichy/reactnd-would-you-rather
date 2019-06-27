4c33486f2baf0c5454efbeff4e1a872a19398419
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-27 14:46:56 -0700

    Feat: Add NavLink to nav.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,9 +6,30 @@ class Nav extends Component {
         return (
             <nav className='nav'>
                 <ul>
-                    <li>Home</li>
-                    <li>New Question</li>
-                    <li>Leader Board</li>
+                    <li>
+                        <NavLink
+                            to='/' exact
+                            activeClassName='active'
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/add'
+                            activeClassName='active'
+                        >
+                            New Question
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/leaderboard'
+                            activeClassName='active'
+                        >
+                            Leader Board
+                        </NavLink>
+                    </li>
                 </ul>
             </nav>
         );
