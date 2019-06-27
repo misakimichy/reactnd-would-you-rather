8e5bdced213e46f7cca793e5bebbc61a385e136b
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-27 14:26:11 -0700

    Feat: Add Router and Route.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
+import LeaderBoard from './LeaderBoard';
+import Login from './Login';
+import NewQuestion from './NewQuestion';
+import QuestionDetail from './QuestionDetail';
 
 class App extends Component {
   componentDidMount() {
@@ -10,20 +15,30 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        {this.props.loggedIn === true
-          ? null
-          : <Dashboard />
-        }
-      </div>
+      <Router>
+        <Fragment>
+          <div className='container'>
+          {this.props.loggedOut === true
+            ? <Route component={Login} />
+            : <div>
+                <Route path='/' exact component={Dashboard} />
+                <Route path='/login' component={Login} />
+                <Route path='/questions/:id' component={QuestionDetail} />
+                <Route path='/add' component={NewQuestion} />
+                <Route path='/leaderboard' component={LeaderBoard} />
+            </div>
+          }
+        </div>
+        </Fragment>
+      </Router>
     );
   }
 }
 
 function mapStateToProps ({ authedUser }) {
   return {
-    loggedIn: authedUser === null
-  }
+    loggedOut: authedUser === null
+  };
 }
 
 export default connect(mapStateToProps)(App);
