a01d644b061b274314e86cce943f36a20e8642aa
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-27 21:18:21 -0700

    Feat: Add question detail page.

--- src/components/QuestionDetail.js
+++ src/components/QuestionDetail.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Question from './Question';
 
 class QuestionDetail extends Component {
     render() {
+        const {id, questions } = this.props;
+        const question = questions[id];
+
         return (
             <div>
-                Question Detail 
+                <h3 className='center'>Question</h3>
+                {question &&
+                    <Question question={question} />}
             </div>
         );
     }
 }
 
-export default QuestionDetail;
+const mapStateToProps = ({ questions }, props) => {
+    const { id } = props.match.params;
+    
+    return {
+        id,
+        questions,
+    };
+}
+
+export default connect(mapStateToProps)(QuestionDetail);
