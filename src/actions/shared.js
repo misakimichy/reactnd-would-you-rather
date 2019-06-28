6e037faa3943ff53690837ff97666ccfd5c2db04
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-27 17:41:17 -0700

    Feat: Dispatch loading bar.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,17 +2,21 @@ import { getInitialData } from '../utils/api';
 import { receiveQuestions} from '../actions/questions';
 import { receiveUsers} from '../actions/users';
 import { setAuthedUser } from '../actions/authedUser';
+import { showLoading, hideLoading } from 'react-redux-loading-bar';
 
 // Todo: Add login screen and select user
 const AUTHED_ID = 'sarah_edo';
 
 export function handleInitialData() {
     return (dispatch) => {
+        dispatch(showLoading())
+
         return getInitialData()
         .then(({ users, questions }) => {
             dispatch(receiveQuestions(questions))
             dispatch(receiveUsers(users))
             dispatch(setAuthedUser(AUTHED_ID))
+            dispatch(hideLoading())
         })
     }
 }
