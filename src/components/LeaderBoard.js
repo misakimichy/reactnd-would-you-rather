f9e466b407428ce087e431e4b7f4f5483078023c
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-28 14:13:58 -0700

    Feat: Remove div unser list.

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -11,19 +11,18 @@ class LeaderBoard  extends Component {
                 <h3 className='center'>Leader Board</h3>
                 <ul className='user-list'>
                     {userArray.map(user =>(
-                        <li key={user}>
-                            <div classNam='user'>
-                                <img
-                                    className='avatar'
-                                    src={user.avatarURL}
-                                    alt={`Avatar of ${user.id}`}
-                                />
-                                <span>{user.name}</span>
-                                <div className='interaction'>
-                                    <p>Number of Asked: {user.questions.length}</p>
-                                    <p>Number of Answered: {Object.keys(user.answers).length}</p>
-                                </div>
-                            </div>
+                        <li
+                            className='user'
+                            key={user}
+                        >
+                            <img
+                                className='avatar'
+                                src={user.avatarURL}
+                                alt={`Avatar of ${user.id}`}
+                            />
+                            <span className='user-name'>{user.name}</span>
+                            <div className='asked'>Number of Asked: {user.questions.length}</div>
+                            <div className='answered'>Number of Answered: {Object.keys(user.answers).length}</div>
                         </li>
                     ))}
                 </ul>
