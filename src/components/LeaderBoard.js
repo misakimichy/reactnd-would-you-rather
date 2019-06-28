2dd9b751a44bd636f1969ca66f6026c18688df4d
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-28 11:56:56 -0700

    Feat: Render leaderboard.

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class LeaderBoard  extends Component {
     render() {
+        const { users } = this.props;
+        const userArray = Object.keys(users).map(key => users[key]);
+
         return (
             <div>
-                Leader Board 
+                <h3 className='center'>Leader Board</h3>
+                <ul className='user-list'>
+                    {userArray.map(user =>(
+                        <li key={user}>
+                            <div classNam='user'>
+                                <img
+                                    className='avatar'
+                                    src={user.avatarURL}
+                                    alt={`Avatar of ${user.id}`}
+                                />
+                                <span>{user.name}</span>
+                                <div className='interaction'>
+                                    <p>Number of Asked: {user.questions.length}</p>
+                                    <p>Number of Answered: {Object.keys(user.answers).length}</p>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
             </div>
         );
     }
 }
 
-export default LeaderBoard;
+function mapStateToProps ({ users }) {
+    return {
+        users
+    }
+}
+
+export default connect(mapStateToProps)(LeaderBoard);
