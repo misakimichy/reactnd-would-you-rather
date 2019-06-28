cf9e9fe39e7e4837a68a1b735a888f26d6b0d30e
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-28 11:36:58 -0700

    Feat: Add handleAddQuestion action.

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,36 @@
+import { showLoading, hideLoading } from 'react-redux-loading-bar';
+import { saveQuestion } from '../utils/api';
+
+export const ADD_QUESTION = 'ADD_QUESTION';
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
+
+function addQuestion(question) {
+    return{
+        type: ADD_QUESTION,
+        question,
+   }
+}
+
+export function handleAddQuestion (optionOne, optionTwo) {
+    return (dispatch, getState) => {
+        const { authedUser } = getState();
+        dispatch(showLoading());
+
+        const questionInfo = {
+            author: authedUser,
+            optionOne,
+            optionTwo
+        }
+
+        return saveQuestion(questionInfo)
+            .then(question => dispatch(addQuestion(question)))
+            .then(() => dispatch(hideLoading()))
+    };
+}
 
 export function receiveQuestions (questions) {
     return {
         type: RECEIVE_QUESTIONS,
         questions,
-    }
+    };
 }
