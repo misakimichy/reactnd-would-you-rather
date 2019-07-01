1ca0080e1ac7286868c5bc249dedb87196d84720
Misaki Koonce <misaki.koonce@gmail.com>
2019-07-01 16:30:28 -0700

    Feat: Add Link to QuestionDetail.

--- src/components/QuestionDetail.js
+++ src/components/QuestionDetail.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Question from './Question';
+import { Link } from 'react-router-dom';
+// import Question from './Question';
 
 class QuestionDetail extends Component {
     render() {
-        const {id, questions } = this.props;
-        const question = questions[id];
+        const { question } = this.props;
+        const { id, optionOne, optionTwo } = question;
 
         return (
-            <div>
-                <h3 className='center'>Question</h3>
-                {question &&
-                    <Question question={question} />}
-            </div>
+            <Link to={`/question/${id}`}>
+                <p>{optionOne.text}</p>
+                <span>or</span>
+                <p>{optionTwo.text}</p>
+            </Link>
         );
     }
 }
 
-const mapStateToProps = ({ questions }, props) => {
-    const { id } = props.match.params;
-    
+const mapStateToProps = ({ questions }, { id }) => {
     return {
-        id,
-        questions,
+        question: questions[id]
     };
 }
 
