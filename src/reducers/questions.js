f2f30af92ea69d2faf13fe52a33a755d2a346e0b
Misaki Koonce <misaki.koonce@gmail.com>
2019-07-01 18:58:09 -0700

    Feat: Remove answer_question reducer. Not needed.

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,8 +1,4 @@
-import {
-    RECEIVE_QUESTIONS,
-    ADD_QUESTION,
-    ANSWER_QUESTION
-} from '../actions/questions';
+import { RECEIVE_QUESTIONS, ADD_QUESTION } from '../actions/questions';
 
 export default function questions (state = {}, action) {
     switch(action.type) {
@@ -17,42 +13,7 @@ export default function questions (state = {}, action) {
                 ...state,
                 ...action.questions
             }
-        
-        case ANSWER_QUESTION :
-            const {authedUser, qid, answer} = action.questionAnswer;
-            let optionOne = state[qid].optionOne;
-            let optionTwo = state[qid].optionTwo;
 
-            if(answer === 'optionOne') {
-                optionOne = {
-                    ...optionOne,
-                    votes: [...optionOne.votes.concat([authedUser])]
-                }
-                optionTwo = {
-                    ...optionTwo,
-                    votes: [...optionTwo.votes.filter(authedId => authedId !== authedId)]
-                }
-            }
-
-            if(answer === 'optionTwo') {
-                optionOne = {
-                    ...optionOne,
-                    votes: [...optionOne.votes.filter(authedId => authedId !== authedId)]
-                }
-                optionTwo = {
-                    ...optionTwo,
-                    votes: [...optionTwo.votes.concat([authedUser])]
-                }
-            }
-            const option = {
-                ...state[qid],
-                optionOne,
-                optionTwo
-            };
-            return {
-                ...state,
-                [qid]:option
-            }
         default :
             return state
     }
