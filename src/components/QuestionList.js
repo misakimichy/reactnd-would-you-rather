7319c4318f6706084dfee1c8d59a806d22397339
Misaki Koonce <misaki.koonce@gmail.com>
2019-07-01 19:09:43 -0700

    File: Rename the file.

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-// import Question from './Question';
 
-class QuestionDetail extends Component {
+class QuestionList extends Component {
     render() {
         const { question } = this.props;
         const { id, optionOne, optionTwo } = question;
 
         return (
-            <Link to={`/question/${id}`}>
-                <p>{optionOne.text}</p>
-                <span>or</span>
-                <p>{optionTwo.text}</p>
+            <Link to={`/question/question_${id}`}>
+                <div class='center qestion-list'>
+                    <p>{optionOne.text}</p>
+                    <span>or</span>
+                    <p>{optionTwo.text}</p>
+                </div>
             </Link>
         );
     }
@@ -24,4 +25,4 @@ const mapStateToProps = ({ questions }, { id }) => {
     };
 }
 
-export default connect(mapStateToProps)(QuestionDetail);
+export default connect(mapStateToProps)(QuestionList);
