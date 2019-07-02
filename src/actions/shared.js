a02a38c162fd86724939426b73bb4d9dd345ed77
Misaki Koonce <misaki.koonce@gmail.com>
2019-07-01 18:52:48 -0700

    Feat: No need to import setAuthedUser.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,7 +1,6 @@
 import { getInitialData } from '../utils/api';
 import { receiveQuestions} from '../actions/questions';
 import { receiveUsers} from '../actions/users';
-import { setAuthedUser } from '../actions/authedUser';
 import { showLoading, hideLoading } from 'react-redux-loading-bar';
 
 export function handleInitialData() {
