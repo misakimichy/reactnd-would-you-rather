e388d1e8ae5797d1d193271a0d1d17eeb4dcdfe3
Misaki Koonce <misaki.koonce@gmail.com>
2019-07-03 20:51:03 -0700

    Feat: Update question sorting and rendering.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,71 +4,62 @@ import QuestionList from './QuestionList';
 
 class Dashboard extends Component {
     state = {
-        activeTab: '1',
+        hasAnswered: false,
     }
 
-    handleTabChange = tab => {
-        if(this.state.activeTab !== tab)
+    handleTabChange = answered => {
         this.setState({
-           activeTab: tab,
-         })
+            hasAnswered: answered
+        })
     }
 
     render() {
-        const { unansweredQId, answeredQId} = this.props;
+        const { hasAnswered } = this.state;
+        const { authedUser, questions } = this.props;
+        const questionsArray = Object.keys(questions).map(key => questions[key]);
+        const filteredQuestions = questionsArray.filter(question => {
+            const contains = (
+                question.optionOne.votes.indexOf(authedUser) > -1 ||
+                question.optionTwo.votes.indexOf(authedUser) > -1
+            );
+            return hasAnswered ? contains : !contains;
+        });
+        const sortedQuestions = filteredQuestions.sort((a, b) => b.timestamp - a.timestamp);
+    
 
         return(
             <div>
-                <h3 className='center'>Question List</h3>
-                <div className='center'>
+                <h1 className='center'>Question List</h1>
+                <div className='center tab'>
                     <button
-                        className='center'
-                        onClick={() => this.handleTabChange('1')}
+                        className={!hasAnswered ? 'button-left active' : 'button-left'}
+                        onClick={() => this.handleTabChange(false)}
                     >
                     Unanswered
                     </button>
                     <button
-                        className='center'
-                        onClick={() => this.handleTabChange('2')}
+                        className={hasAnswered ? 'button-right active' : 'button-right'}
+                        onClick={() => this.handleTabChange(true)}
                     >
                     Answered
                     </button>
                 </div>
-                <div>
-                    <ul className='question-list'>
-                        {(unansweredQId.map(questionId => (
-                            <li key={questionId}>
-                                <QuestionList id={questionId} />
-                            </li>
-                        )))}
-                    </ul>
-                    <ul className='question-list'>
-                        {answeredQId.map(questionId => (
-                            <li key={questionId}>
-                                <QuestionList id={questionId} />
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+                <ul>
+                    {sortedQuestions.map(question => (
+                        <li key={question.id}>
+                        <QuestionList id={question.id} />
+                        </li>
+                    ))}
+                </ul>
             </div>
         )
     }
 }
 
-function mapStateToProps ({ questions, authedUser}) {
-    const unanswered = Object.values(questions).filter(question =>
-        !question.optionOne.votes.includes(authedUser) &&
-            !question.optionTwo.votes.includes(authedUser));
-
-    const answered = Object.values(questions).filter(question =>
-        question.optionOne.votes.includes(authedUser) ||
-            question.optionTwo.votes.includes(authedUser));
-
+function mapStateToProps ({ authedUser, questions }) {
     return {
-        unansweredQId: Object.values(unanswered)
-            .sort((a, b) => b.timestamp - a.timestamp).map(q => q.id),
-        answeredQId: Object.values(answered)
-            .sort((a, b) => b.timestamp - a.timestamp).map(q => q.id)
+        authedUser,
+        questions,
     }
 }
 
