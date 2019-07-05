4a664c10e90b4e3040a3f28154c9d3b885f8621e
Misaki Koonce <misaki.koonce@gmail.com>
2019-07-05 11:38:09 -0700

    Feat: import bootstrap for reactstrap.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.css';
 import reducer from './reducers';
 import './index.css';
 import App from './components/App';
