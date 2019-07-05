1b8786ddc86d91ace78eb0c47f0a15a6b9ada2ab
Misaki Koonce <misaki.koonce@gmail.com>
2019-07-05 11:37:16 -0700

    Feat: Import reactstrap.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,65 +1,83 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Nav, NavItem, NavLink, TabContent, TabPane } from 'reactstrap';
+import classnames from 'classnames';
 import QuestionList from './QuestionList';
 
 class Dashboard extends Component {
     state = {
-        hasAnswered: false,
+        activeTab : '1'
     }
 
-    handleTabChange = answered => {
-        this.setState({
-            hasAnswered: answered
-        })
+    handleTabChange = tab => {
+        if (this.state.activeTab !== tab) {
+            this.setState({
+                activeTab: tab
+            });
+        }
     }
 
     render() {
-        const { hasAnswered } = this.state;
-        const { authedUser, questions } = this.props;
-        const questionsArray = Object.keys(questions).map(key => questions[key]);
-        const filteredQuestions = questionsArray.filter(question => {
-            const contains = (
-                question.optionOne.votes.indexOf(authedUser) > -1 ||
-                question.optionTwo.votes.indexOf(authedUser) > -1
-            );
-            return hasAnswered ? contains : !contains;
-        });
-        const sortedQuestions = filteredQuestions.sort((a, b) => b.timestamp - a.timestamp);
+        const { unansweredQids, answeredQids } = this.props;
     
-
         return(
+            // reactstrap nav: https://reactstrap.github.io/components/navs/
+            // reactstarp tabs: https://reactstrap.github.io/components/tabs/
             <div>
-                <h1 className='center'>Question List</h1>
-                <div className='center tab'>
-                    <button
-                        className={!hasAnswered ? 'button-left active' : 'button-left'}
-                        onClick={() => this.handleTabChange(false)}
-                    >
-                    Unanswered
-                    </button>
-                    <button
-                        className={hasAnswered ? 'button-right active' : 'button-right'}
-                        onClick={() => this.handleTabChange(true)}
-                    >
-                    Answered
-                    </button>
-                </div>
-                <ul>
-                    {sortedQuestions.map(question => (
-                        <li key={question.id}>
-                        <QuestionList id={question.id} />
-                        </li>
-                    ))}
-                </ul>
+                <Nav tabs>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({active: this.state.activeTab === '1'})}
+                            onClick={() => this.handleTabChange('1')}
+                        >
+                            Unanswered
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({active: this.state.activeTab === '2'})}
+                            onClick={() => this.handleTabChange('2')}
+                        >
+                            See Answered
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                <TabContent activeTab={this.state.activeTab}>
+                    <TabPane tabId='1'>
+                        <ul>
+                            {unansweredQids.map(questionId => (
+                                <li key={questionId}>
+                                    <QuestionList id={questionId}/>
+                                </li>
+                            ))}
+                        </ul>
+                    </TabPane>
+                    <TabPane tabId='2'>
+                        <ul>
+                            {answeredQids.map(questionId => (
+                                <li key={questionId}>
+                                    <QuestionList id={questionId} />
+                                </li>
+                            ))}
+                        </ul>
+                    </TabPane>
+                </TabContent>
             </div>
         )
     }
 }
 
 function mapStateToProps ({ authedUser, questions }) {
+    const unansweredQuestions = Object.values(questions).filter(question =>
+        !question.optionOne.votes.includes(authedUser) && !question.optionTwo.votes.includes(authedUser)
+    );
+    const answeredQuestions = Object.values(questions).filter(question =>
+        question.optionOne.votes.includes(authedUser) || question.optionTwo.votes.includes(authedUser)
+    );
+
     return {
-        authedUser,
-        questions,
+        unansweredQids: Object.values(unansweredQuestions).sort((a, b) => b.timestamp - a.timestamp).map(q => q.id),
+        answeredQids: Object.values(answeredQuestions).sort((a, b) => b.timestamp - a.timestamp).map(q => q.id),
     }
 }
 
