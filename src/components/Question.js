f72e812907a32f718f181f65713a1ccd61d5f634
Misaki Koonce <misaki.koonce@gmail.com>
2019-07-06 11:32:59 -0700

    Style: Add style for Question component.

--- src/components/Question.js
+++ src/components/Question.js
@@ -6,7 +6,7 @@ import { handleAnswerQuestion } from '../actions/questions';
 
 class Question extends Component {
     state = {
-        vote: false,
+        answer: false,
     }
     
     handleAnswer = answer => {
@@ -15,41 +15,53 @@ class Question extends Component {
     }
 
     render() {
-        const { question } = this.props;
+        const { users, question } = this.props;
         
         return (
             <Fragment>
                 {question
-                    ? <div>
-                            <h1>Would You Rather</h1>
-                            <div>{question.author}</div>
-                            <div>
-                                <QuestionDetail
-                                    questionId={question.id}
-                                    optionName="optionOne"
-                                    onClick={this.handleAnswer}
-                                />
-                                <QuestionDetail
-                                    questionId={question.id}
-                                    optionName="optionTwo"
-                                    onClick={this.handleAnswer}
-                                />
-                            </div>
+                ? <div>
+                    <h1 className='center'>This is Question Component</h1>
+                    <div className='question-card'>
+                        <div className='author-info'>
+                            <img
+                                className='question-card-avatar'
+                                src={users[question.author].avatarURL}
+                                alt={`avatar of ${question.author}`}
+                            />
+                            <div className='question-author'>{question.author}</div>
+                        </div>
+                        <div className='option-container'>
+                            <QuestionDetail
+                                questionId={question.id}
+                                optionName="optionOne"
+                                onClick={this.handleAnswer}
+                            />
+                            <span>or</span>
+                            <QuestionDetail
+                                questionId={question.id}
+                                optionName="optionTwo"
+                                onClick={this.handleAnswer}
+                            />
+                        </div>
                     </div>
-                    : <NotFound />
-                }
+                </div>
+                : <NotFound />
+
+            }
             </Fragment>
         )
     }
 }
 
-function mapStateToProps({ authedUser, questions }, props) {
+function mapStateToProps({ authedUser, questions, users }, props) {
     const { question_id } = props.match.params;
     const question = questions[question_id];
     
     return {
         authedUser,
         question,
+        users,
     };
 }
 
