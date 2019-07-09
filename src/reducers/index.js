9b4fcc3923b8962bf3eec4ccb546d51ac8f7493e
Misaki Koonce <misaki.koonce@gmail.com>
2019-07-08 17:32:34 -0700

    Style: Update whitespace.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import authedUser from './authedUser';
 import users from './users';
 import questions from './questions';
