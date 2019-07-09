6b57fb5f9cbfd2720e6650a3b83af009218fae6a
Misaki Koonce <misaki.koonce@gmail.com>
2019-07-08 19:38:47 -0700

    Style: Organize the file.

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -12,6 +12,22 @@ function addQuestion(question) {
    }
 }
 
+export function receiveQuestions (questions) {
+    return {
+        type: RECEIVE_QUESTIONS,
+        questions,
+    };
+}
+
+export function addQuestionAnswer(authedUser, qid, answer) {
+    return {
+        type: ADD_QUESTION_ANSWER,
+        authedUser,
+        qid,
+        answer,
+    }
+}
+
 export function handleAddQuestion (optionOneText, optionTwoText) {
     return (dispatch, getState) => {
         const { authedUser } = getState();
@@ -24,22 +40,6 @@ export function handleAddQuestion (optionOneText, optionTwoText) {
         }).then(question => dispatch(addQuestion(question)))
             .then(() => dispatch(hideLoading()))
     };
-}
-
-export function receiveQuestions (questions) {
-    return {
-        type: RECEIVE_QUESTIONS,
-        questions,
-    };
-}
-
-function addQuestionAnswer(authedUser, qid, answer) {
-    return {
-        type: ADD_QUESTION_ANSWER,
-        authedUser,
-        qid,
-        answer,
-    }
 }
 
 export function handleAnswerQuestion (qid, answer) {
